84ff120f03986da7827240f390b51c254769f1de
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-12-30 23:15:11 +0100

    Update index.js

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+let todoInput; //miejesce gdzie użytkowanik wpisuje treść zadania
+let errorInfo; //informacje o braku zadań/konieczność wpsiania do listy
+let addBtn; //przycisk dodaje nowe elemnty do listy;
+let ulList; // lista zadań tagi ul
