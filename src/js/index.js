bae192d0e1d86ef5f9cef403a0b7d301c23a7484
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-12-30 23:23:53 +0100

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,22 @@ let todoInput; //miejesce gdzie użytkowanik wpisuje treść zadania
 let errorInfo; //informacje o braku zadań/konieczność wpsiania do listy
 let addBtn; //przycisk dodaje nowe elemnty do listy;
 let ulList; // lista zadań tagi ul
+
+
+const main = () => {
+    prepareDOMElements();
+    prepareDOMEvents();
+}
+
+const prepareDOMElements = () => {
+    todoInput = document.querySelector('.todo-input');
+    errorInfo = document.querySelector('.error-info');
+    addBtn = document.querySelector('.btn-add');
+    ulList = document.querySelector('.todolist ul');
+}
+
+const prepareDOMEvents = () => {
+    
+}
+
+document.addEventListener('DOMContentLoaded', main);
