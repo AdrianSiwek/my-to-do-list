b035d5fb647f66836adc52ef812d6b6ec002eeb1
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-12-31 01:38:45 +0100

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ let todoInput; //miejesce gdzie użytkowanik wpisuje treść zadania
 let errorInfo; //informacje o braku zadań/konieczność wpsiania do listy
 let addBtn; //przycisk dodaje nowe elemnty do listy;
 let ulList; // lista zadań tagi ul
+let newTodo;
 
 
 const main = () => {
@@ -19,18 +20,61 @@ const prepareDOMElements = () => {
 
 
 const prepareDOMEvents = () => {
-    addBtn.addEventListener('clisk', newTask);
+    addBtn.addEventListener('click', addNewTodo);
+    ulList.addEventListener('click', checkClick);
 }
 
 
 
-const newTask = () => {
+const addNewTodo = () => {
     if (todoInput.value !== '') {
-        console.log('ok')
+        newTodo = document.createElement('li');
+        newTodo.textContent = todoInput.value;
+        createDivTools()
+        ulList.append(newTodo);
+        todoInput.value = '';
+        errorInfo.textContent = '';
     } else {
         errorInfo.textContent = 'Wpisz treść zadania';
     }
 }
 
+const createDivTools = () => {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('tools');
+    newTodo.append(newDiv);
+
+    const completeBtn = document.createElement('button');
+    newDiv.appendChild(completeBtn);
+    completeBtn.classList.add('complete');
+    completeBtn.innerHTML = '<i class="fas fa-check"></i>'
+
+    const editBtn = document.createElement('button');
+    newDiv.appendChild(editBtn);
+    editBtn.classList.add('edit');
+    editBtn.textContent = 'EDIT';
+
+    const deleteBtn = document.createElement('button');
+    newDiv.appendChild(deleteBtn);
+    deleteBtn.classList.add('delete');
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
+
+    //newDiv.append(completeBtn, editBtn, deleteBtn);
+
+}
+
+
+const checkClick = e => {
+    if (e.target.matches('.complete')) {
+        e.target.closest('li').classList.toggle('complete');
+        e.target.classList.toggle('complete');
+    } else if (e.target.matches('.edit')) {
+        e.target.closest('li').classList.toggle('edit');
+        e.target.classList.toggle('edit');
+    } else if (e.target.matches('.delete')) {
+        e.target.closest('li').classList.toggle('delete');
+        e.target.classList.toggle('delete');
+    }
+}
 
 document.addEventListener('DOMContentLoaded', main);
